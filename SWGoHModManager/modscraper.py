54ce6b9c2191cb2f6bb4a101b450f29f4eb27bf9
Tres Serio <treserio@hotmail.com>
2017-11-29 17:23:12 -0600

    Scraprogress
    Need to figure out hasattr for ("a", {"aria-label": "Next"}) to handle pagenation.

--- SWGoHModManager/modscraper.py
+++ SWGoHModManager/modscraper.py
@@ -2,6 +2,8 @@ import requests
 from bs4 import BeautifulSoup
 
 url = "https://swgoh.gg/u/sert/mods/"
+url_page_2 = "https://swgoh.gg/u/sert/mods/?page=2"
+
 r = requests.get(url)
 
 soup = BeautifulSoup(r.content)
@@ -9,12 +11,8 @@ soup = BeautifulSoup(r.content)
 mods1 = soup.find_all("div",{"class": "statmod-stats-1"})
 mods2 = soup.find_all("div",{"class": "statmod-stats-2"})
 
-print(mods[0].find_all("span", {"class": "statmod-stat-label"})[0].text)
-
-for item in mods:
-    print(item)
-
-for item in mods:
+#prints 2ndary mod values in order
+for item in mods2:
     print(item.find_all("span", {"class": "statmod-stat-label"})[0].text)
     print(item.find_all("span", {"class": "statmod-stat-value"})[0].text)
     print(item.find_all("span", {"class": "statmod-stat-label"})[1].text)
@@ -24,10 +22,23 @@ for item in mods:
     print(item.find_all("span", {"class": "statmod-stat-label"})[3].text)
     print(item.find_all("span", {"class": "statmod-stat-value"})[3].text)
 
-for item in mods:
-    print(item.contents[1].text)
+#Mods per page with correlated information, only thing missing is the shape of the mod and type.../sigh check image type for correlation, learn to if/then in the loop for python.../cry
+i=0
+for item in mods1:
+    print(item.find_all("span", {"class": "statmod-stat-label"})[0].text)
+    print(item.find_all("span", {"class": "statmod-stat-value"})[0].text)
+    print(mods2[i].find_all("span", {"class": "statmod-stat-label"})[0].text)
+    print(mods2[i].find_all("span", {"class": "statmod-stat-value"})[0].text)
+    print(mods2[i].find_all("span", {"class": "statmod-stat-label"})[1].text)
+    print(mods2[i].find_all("span", {"class": "statmod-stat-value"})[1].text)
+    print(mods2[i].find_all("span", {"class": "statmod-stat-label"})[2].text)
+    print(mods2[i].find_all("span", {"class": "statmod-stat-value"})[2].text)
+    print(mods2[i].find_all("span", {"class": "statmod-stat-label"})[3].text)
+    print(mods2[i].find_all("span", {"class": "statmod-stat-value"})[3].text)
+    i+=1
+    print("")
 
-
+#.text for text, and .replace('x','y') to replace a character
 #statmod-stats-1 class for primary
 #statmod-stats-2 class for secondary
 #statmod-stat-value = number
@@ -36,3 +47,26 @@ for item in mods:
 #first loop goes through both 1 and 2, 2 is x4 of 1
 #statmod-stat-label in array is string to compare with json values for placement
 #append values to array? one for values one for labels?
+#save printed info into variables, append to arrays preferrably
+
+#for pagenation
+#if ("a", {"aria-label":"next"}) = true
+#    url_page_i = "https://swgoh.gg/u/sert/mods/?page=i"
+#else end
+
+N=16
+nxt = soup.find_all("a", {"aria-label": "Next"})[0].text
+#If the above is not None...it errors trying to set it if it's none, need a way to find it before hand.
+#find beautiful soup to check if an element tag exists
+if nxt is not None:
+    url = url + '?page=' + str(N)
+    print(url + '   ' + str(N))
+    N += 1
+
+if soup.hasattr(("aria-label")):
+    print(soup.find_all("a", {"aria-label": "Next"}))
+
+
+print(soup.find_all("a", {"aria-label": "Next"}))
+
+page = soup.find_all("ul", {"class": "pagination"})
