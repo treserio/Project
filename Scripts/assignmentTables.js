e903465c95b27222e9628e234b99705ee2bcd4b8
Tres Serio <treserio@hotmail.com>
2017-12-06 17:31:53 -0600

    Jscript Tables
    Are finished, may need to put them into a function to narrow scope, but for now they work. Working on autocomplete for membrName inputs based on json file to auto fill associated tables.

--- Scripts/assignmentTables.js
+++ Scripts/assignmentTables.js
@@ -1,22 +1,38 @@
-// create a table creation method?
-//create dom and it should show up, could push to array after created and then iterate to display also?
+//$document ready function
 var sqdtbls = [];
+//Creates an array of tables to allow users to drag mods from the top table to the lower in order to customize sets. When it's assigned to a lower table it should color the associated row in the top.
+var nameselect = [];
+//Creates an array of inputs elements to use just above the tables. These are to get an input that will search the data and populate the associated table with the correct mods for the character name selected.
 
-
-for (var i = 0; i<6; i++){
-    sqdtbls.push((document.createElement("table")));
-    $(sqdtbls[i]).attr( {
-        "id": "Membr"+(i+1),
-        "class": "display assigned",
-        "width": "100%"
+for (var i = 0; i<5; i++){
+    nameselect.push(document.createElement('input'));
+    $(nameselect[i]).attr({
+        'id': 'membrName'+i,
+        'type': 'text'
     });
-    //var hdr = document.createElement("thead")
-    //sqdtbls[i].getElementById('Membr'+i).appendChild(hdr)
-    //var tr = document.createElement("TR")
-    //document.getElementById()
+    sqdtbls.push(document.createElement('table'));
+    $(sqdtbls[i]).attr( {
+        'id': 'modSet'+i,
+        'class': 'display assigned',
+        'width': '100%'
+    });
+    sqdtbls[i].createTHead();
+    sqdtbls[i].lastChild.innerHTML = '<tr><th width=\'90px\'>Equipped</th><th>Slot</th><th width=\'40px\'>Set</th><th>Primary<th><th>Spd</th><th>Crit%</th><th>Off</th><th>Off%</th><th>Prot</th><th>Prot%</th><th>HP</th><th>HP%</th><th>Def</th><th>Def%</th><th>Pot%</th><th>Ten%</th></tr>';
+    sqdtbls[i].createTBody();
+    sqdtbls[i].lastChild.insertAdjacentHTML('beforeend', '<tr id=\'square\'><td></td><td>Square</td></tr>');
+    sqdtbls[i].lastChild.insertAdjacentHTML('beforeend', '<tr id=\'arrow\'><td></td><td>Arrow</td></tr>');
+    sqdtbls[i].lastChild.insertAdjacentHTML('beforeend', '<tr id=\'diamond\'><td></td><td>Diamond</td></tr>');
+    sqdtbls[i].lastChild.insertAdjacentHTML('beforeend', '<tr id=\'triangle\'><td></td><td>Triangle</td></tr>');
+    sqdtbls[i].lastChild.insertAdjacentHTML('beforeend', '<tr id=\'circle\'><td></td><td>Circle</td></tr>');
+    sqdtbls[i].lastChild.insertAdjacentHTML('beforeend', '<tr id=\'cross\'><td></td><td>Cross</td></tr>');
 
-    //sqdtbls[i].
-    //$('#squadMembr'+i).append(sqdtbl[i]);
-    //document.getElementById("squadMembr"+i) = sqdtbls[i];
+    document.getElementById('squadMembr'+i).appendChild(nameselect[i]);
+    document.getElementById('squadMembr'+i).appendChild(sqdtbls[i]);
 }
-console.log(sqdtbls)
+//function for grabbing correct mod data from JSON file with search field input.
+
+$('#membrName0').on('keyup change', function() {
+    var namesearch = $('#membrName0').val();
+    console.log(namesearch);
+}
+)
