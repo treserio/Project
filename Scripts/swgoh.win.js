ee781fbc4545ad8ec11bd13b9d6480e1929c8518
Tres Serio <treserio@hotmail.com>
2018-01-12 09:27:24 -0600

    DocumentRdy
    Enclosed everything in a document ready json file

--- Scripts/swgoh.win.js
+++ Scripts/swgoh.win.js
@@ -1,5 +1,4 @@
-//$document ready function...now that I've written all of this I find myself wondering why I didn't just use datatables to do it to begin with.
-
+$(document).ready(function() {
 //Creates an array of tables to allow users to drag mods from the top table to the lower in order to customize sets. When it's assigned to a lower table it should color the associated row in the top.
 var sqdTbls = [];
 //Creates an array of inputs elements to use just above the tables. These are to get an input that will search the data and populate the associated table with the correct mods for the character name selected.
@@ -9,6 +8,7 @@ var hideBtn = [];
 //Creates an array of buttons to run a clear function on the appropriate color tbl
 var clrBtn = [];
 
+var filelocation = '';
 // i counter for 5 tables, j counter for 6 rows + headers, k counter for 16 cells in each row
 for (var i = 0; i<5; i++) {
     // Name input field for autocomplete and population of table
@@ -121,7 +121,43 @@ for (var i = 0; i<5; i++) {
     // Apply it to the right squadMembr div
     document.getElementById('squadMembr'+i).appendChild(colorTbl);
 }
+$('#srchForm').submit(function(event){
+    event.preventDefault();
+    // Grab the search text
+    srchstring = $('#srchString').val();
+    srchstring = srchstring.toLowerCase();
+    filelocation = 'AcctData/' +srchstring+ '.json';
+    
+    // check if resulting url is good? no good method with CORS that'll work. Possibly in an NPM package, yay I have a sticker for that.
+    if (srchstring == "") { 
+        alert('Please enter a swgoh.gg id');
+        
+    } else {
+        $.getJSON('AcctData/' +srchstring+ '.json', function(jsonfile) {
+            // Display the hidden dataTable
+            $('.dtWrap').css('display', 'block');
+            $('.assignBtnWrap').css('display','block')
+            // Populate the dataTable
+            drawDataTbl(srchstring);
 
+            // Setup Autocomplete, first create array of names, then apply to autocomplete.
+            var namelist = [];
+            for (var key in jsonfile.data) {
+                //check for unique entries and add to the namelist array for setting autocomplete
+                if ($.inArray(jsonfile.data[key].Equipped, namelist) === -1) {
+                    namelist.push(jsonfile.data[key].Equipped);
+                }
+            }
+            //Apply autocomplete list to input fields, 0 to 4
+            $('[id^=membrName').autocomplete( {
+                source: namelist
+            });
+        //if .getJSON fails run the python script to create the file, probably should move autocomplete out of this function.
+        }).fail(function(data) {
+            alert('Please contact Sert#7357 on discord\nin order to have your mod data collected');
+        })
+    }
+});
 //function for grabbing correct mod data from JSON file with autocomplete field input.
 $('[id^=membrName').on('autocompleteclose', function() {
     var namesearch = this.value;
@@ -930,4 +966,112 @@ $('[id^=clrBtn').on('click', function() {
     DTRedraw();
     $('#squadMembr'+id+' .assigned tbody').css('display','none');
     $('[id^=hideShow'+id).attr('value','Hide');
+});
+
+// This fills an existing table with data supplied through the screen scraping of www.swgoh.gg into a json file, the swgoh.gg account name should match the one used for this site.
+function drawDataTbl (search) {
+    $('#moddata').DataTable( {
+        iDisplayLength: 10,
+        lengthMenu: [[10,30,50,-1],[10,30,50,"All"]],
+        destroy: true,  //rewrites the datatable
+        scrollY: '300px',
+        scrollX: '100%',
+        paging: false,
+        //bInfo: false,
+        ajax: {
+            url: 'AcctData/' +search+ '.json',
+        },
+        dom: 'ltip', //removes top search bar
+        // should enable a fixed column on the left, but isn't working command = fixedColums:
+        // hiding the Assigned column
+        columnDefs: [ {
+            targets: [16],
+            visible: false,
+            searchable: false
+        } ],
+        // data: 'titles' have to match <th> elements in the target table
+        columns: [
+            { data: 'Equipped', width: '90px'},
+            { data: 'Slot'  },
+            { data: 'Set', width: '40px' },
+            { data: 'Primary' },
+            { data: 'Spd',
+            defaultContent: "", },
+            { data: 'Crit%',
+            defaultContent: "" },
+            { data: 'Off',
+            defaultContent: "" },
+            { data: 'Off%',
+            defaultContent: "" },
+            { data: 'Prot',
+            defaultContent: "" },
+            { data: 'Prot%',
+            defaultContent: "" },
+            { data: 'HP',
+            defaultContent: "" },
+            { data: 'HP%',
+            defaultContent: "" },
+            { data: 'Def',
+            defaultContent: "" },
+            { data: 'Def%',
+            defaultContent: "" },
+            { data: 'Pot%',
+            defaultContent: "" },
+            { data: 'Ten%',
+            defaultContent: "" },
+            { data: 'Assigned',
+            defaultContent: "" }
+        ],
+        // Function for coloring table rows based on assigned values
+        rowCallback: function( row, data, index ) {
+            if ( data.Assigned == "" ) {
+                $('td', row).css("background-color","");
+            } else if ( data.Assigned == "1st" ) {
+                $('td', row).css("background-color","lightblue");
+            } else if ( data.Assigned == "2nd" ) {
+                $('td', row).css("background-color","rgba(144, 238, 144, 0.788)");
+            } else if ( data.Assigned == "3rd" ) {
+                $('td', row).css("background-color","rgba(255, 166, 0, 0.850)");
+            } else if ( data.Assigned == "4th" ) {
+                $('td', row).css("background-color",'rgba(189, 96, 189, 0.788)');
+            } else if ( data.Assigned == "5th" ) {
+                $('td', row).css("background-color",'rgba(245, 245, 113, 0.780)');
+            }
+        }
+    });
+//select filter search from drop down, try to find again list? This would be for those with only ~6 options
+
+     //Setup - add a text input to each footer cell
+    $('.dataTables_scrollFootInner srch').each( function () {
+        var title = $(this).text();
+        $(this).html( '<input type="text" placeholder="Search '+title+'" />' );
+    });
+
+    var table = $('#moddata').DataTable();
+    
+    // Apply the filter
+    table.columns().every( function () {
+        var column = this;
+
+        $( 'input', this.footer() ).on( 'keyup change', function () {
+            column
+                .search(this.value, true, false)
+                .draw();
+        } );
+    } );
+    
+    //Collecting row data on click, will also need a popup to determine where it goes.
+    $('.dataTable').on('click', 'tbody tr', function() {
+        //Need id to be assigned by clicking on an option and we're done.
+        id = $("input[name='modset']:checked").val();
+        modArray = [];
+        modArray.push(table.row(this).data());
+        redraw(modArray, id);
+        CalcTotals(id);
+        $('#squadMembr'+id+' .assigned tbody').css('display','table-row-group');
+        $('[id^=hideShow'+id).attr('value','Hide');
+        recolorSngl(modArray[0].Equipped, id, modArray[0].Slot);
+        DTRedraw();
+    });
+}
 });
